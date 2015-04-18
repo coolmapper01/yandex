5b9370e9aaf7bdc38f7c600e79ff125d188297c6
coolmapper01 <coolmapper01@yandex.com>
2015-04-18 08:26:28 +0200

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,14 @@
+{
+"moduleId" : "coolmapper01/yandex",
+"title" : "yandex",
+"subtitle" : "www.yandex.com",
+"backButton" : true,
+"enableGPS" : false,
+"zoomControl" : false,
+"screenLockRot" : false,
+"reload" : false,
+"shouldOverrideUrlLoading1" : "http",
+"shouldOverrideUrlLoading2" : "https",
+"loadUrl" : "http://maps.yandex.ru/?ll=#lng#%2C#lat#&z=#zoom#&l=map%2Ctrf",
+"browserLaunchLink" : "http://maps.yandex.ru/?ll=#lng#%2C#lat#&z=#zoom#&l=map%2Ctrf"
+}
